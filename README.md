4977ed667515f6b2e2a7c4ba1de973ad52e39e1b
Emanuele Ruffaldi <emanuele.ruffaldi@gmail.com>
2016-06-28 20:27:51 +0200

    initial from FreenectDriver, not yet built

---
+++ README.md
@@ -0,0 +1,11 @@
+OpenNI2-rosDriver
+=================
+
+OpenNI2-rosDriver is a bridge to ROS implemented as an OpenNI2 driver.
+
+
+Phase 1:
+- build mock
+
+Phase 2:
+- everything fixed
---
+++ src/ColorStream.cpp
@@ -0,0 +1,90 @@
+#include <string>
+#include "ColorStream.hpp"
+
+using namespace RosDriver;
+
+
+ColorStream::ColorStream(void* pDevice) : VideoStream(pDevice)
+{
+  video_mode = makeOniVideoMode(ONI_PIXEL_FORMAT_RGB888, 640, 480, 30);
+  setVideoMode(video_mode);
+  pDevice->startVideo();
+}
+
+#if 0
+// Add video modes here as you implement them
+ColorStream::FreenectVideoModeMap ColorStream::getSupportedVideoModes()
+{
+  FreenectVideoModeMap modes;
+  //                    pixelFormat, resolutionX, resolutionY, fps    freenect_video_format, freenect_resolution
+  modes[makeOniVideoMode(ONI_PIXEL_FORMAT_RGB888, 640, 480, 30)] = std::pair<freenect_video_format, freenect_resolution>(FREENECT_VIDEO_RGB, FREENECT_RESOLUTION_MEDIUM);
+
+
+  return modes;
+
+  /* working format possiblities
+  FREENECT_VIDEO_RGB
+  FREENECT_VIDEO_YUV_RGB
+  FREENECT_VIDEO_YUV_RAW
+  */
+}
+#endif
+
+OniStatus ColorStream::setVideoMode(OniVideoMode requested_mode)
+{
+#if 0
+  FreenectVideoModeMap supported_video_modes = getSupportedVideoModes();
+  FreenectVideoModeMap::const_iterator matched_mode_iter = supported_video_modes.find(requested_mode);
+  if (matched_mode_iter == supported_video_modes.end())
+#endif
+    return ONI_STATUS_NOT_SUPPORTED;
+
+#if 0
+  freenect_video_format format = matched_mode_iter->second.first;
+  freenect_resolution resolution = matched_mode_iter->second.second;
+
+  try { device->setVideoFormat(format, resolution); }
+  catch (std::runtime_error e)
+  {
+    LogError("Format " + to_string(format) + " and resolution " + to_string(resolution) + " combination not supported by libfreenect");
+    return ONI_STATUS_NOT_SUPPORTED;
+  }
+  video_mode = requested_mode;
+  return ONI_STATUS_OK;
+#endif
+}
+
+void ColorStream::populateFrame(void* data, OniFrame* frame) const
+{
+  frame->sensorType = sensor_type;
+  frame->stride = video_mode.resolutionX * 3;
+  frame->cropOriginX = 0;
+  frame->cropOriginY = 0;
+  frame->croppingEnabled = false;
+
+  // copy stream buffer from freenect
+  switch (video_mode.pixelFormat)
+  {
+    default:
+      LogError("Pixel format " + to_string(video_mode.pixelFormat) + " not supported by populateFrame()");
+      return;
+
+    case ONI_PIXEL_FORMAT_RGB888:
+      uint8_t* source = static_cast<uint8_t*>(data);
+      uint8_t* target = static_cast<uint8_t*>(frame->data);
+      std::copy(source, source + frame->dataSize, target);
+      return;
+  }
+}
+
+/* color video modes reference
+
+FREENECT_VIDEO_RGB             = 0, //< Decompressed RGB mode (demosaicing done by libfreenect)
+FREENECT_VIDEO_BAYER           = 1, //< Bayer compressed mode (raw information from camera)
+FREENECT_VIDEO_YUV_RGB         = 5, //< YUV RGB mode
+FREENECT_VIDEO_YUV_RAW         = 6, //< YUV Raw mode
+
+ONI_PIXEL_FORMAT_RGB888 = 200,
+ONI_PIXEL_FORMAT_YUV422 = 201,
+ONI_PIXEL_FORMAT_JPEG = 204,
+*/
